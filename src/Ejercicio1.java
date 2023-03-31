447f66ee43c296c48058d458ef219cf6d115ef25
m.escribano.verde <maria.escribanoverde@gmail.com>
2023-03-31 10:10:52 +0200

    Prueba de modificación en código

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,6 +1,5 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-
         int[] p1 = {1,2,3,4};
         int[] p2 = {6, 4, 1};
         int[] p3 = {2, 4, 5, 6};
@@ -8,10 +7,10 @@ public class Ejercicio1 {
         System.out.println(verificar(p1));
         System.out.println(verificar(p2));
         System.out.println(verificar(p3));
-
     }
 
     public static boolean verificar(int[] prueba){
+        //Verificar si empieza o termina por 6
         int longitud = prueba.length;
         return (prueba[0] == 6 || prueba[longitud - 1] == 6);
     }
